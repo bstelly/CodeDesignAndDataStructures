177e3ce53c5bd8e17d67f7df190e80bc9161990b
bstelly <bstelly1999@gmail.com>
2017-12-19 17:50:08 -0600

    Worked on classes

--- LinkedLists/LinkedListType.h
+++ LinkedLists/LinkedListType.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "LinkedListIterator.h"
 #include <ostream>
+#include <assert.h>
 
 template<class Type>
 class LinkedListType
@@ -12,16 +13,16 @@ protected:
 
 public:
 	//Overload the assignment operator
-	const LinkedListType<Type>& operator=(const LinkedListType<Type>&)
+	const LinkedListType<Type>& operator=(const LinkedListType<Type> &rhs)
 	{
-		
+
 	}
 
 	//Initialize the list to an empty state
 	//Postcondition: first = NULL, count = 0
 	void InitializeList()
 	{
-		
+		DestroyList();
 	}
 
 	//Funtion to determine whether the list is empty
@@ -47,26 +48,22 @@ public:
 	//Postcondition: The value of count is returned
 	int Length()
 	{
-		NodeType<Type> *temp = first;
-		int iter = 0;
-		while (temp != NULL)
-		{
-			iter++;
-			temp = temp->link;
-		}
-		return iter;
+		return count;
 	}
 
 	//Function to delete all the nodes from the list
 	//Postcondition: first = NULL, last = NULL, count = 0
 	void DestroyList()
 	{
-		NodeType<Type> *temp = first;
-		while (temp != NULL)
+		NodeType<Type> *temp;
+		while (first != NULL)
 		{
+			temp = first;
+			first = first->link;
 			delete temp;
-			temp = temp->link;
 		}
+		last = NULL;
+		count = 0;
 	}
 
 	//Function to return the first element of the list
@@ -75,7 +72,8 @@ public:
 	//element of the list is required
 	Type Front()
 	{
-		
+			assert(first != NULL);
+			return first;
 	}
 
 	//Function to return the last element of the list
@@ -84,55 +82,60 @@ public:
 	//element of the list is returned
 	Type Back()
 	{
-		
+			assert(last != NULL);
+			return last;
 	}
 
 	//Function to determine whether searchItem is in the list
 	//Postcondition: Returns true if searchItem is in the list,otherwise the value false
 	//is returned
-	bool Search(const Type&) const
+	bool Search(const Type &rhs) const
 	{
-		
+
 	}
 
 	//Function to insert newItem at the beginning of the list
 	//Postcondition: First points to the new list, newItem is inserted at the beginning
 	//of the list, last points to the last node in the list and count is incremented
 	//by one
-	void InsertFirst(const Type&)
+	void InsertFirst(const Type &rhs)
 	{
-		
+		NodeType<Type>
 	}
 
 	//Function to insert newItem at the end of the list
 	//Postcondition: first points to the new list, newItem is inserted at the end of the
 	//list, last points to the last node in the list and count is incremented by one
-	void InsertLast(const Type&)
+	void InsertLast(const Type &rhs)
 	{
-		
+
 	}
 
 	//Function to delete deleteItem from the list
 	//Postcondition: If found, the node containing deleteItem is deleted from the list.
 	//First points to the first node, list points to the last node of the updated list,
 	//and counts is decremented by one
-	void DeleteNode(const type&)
+	void DeleteNode(const Type &rhs)
 	{
-		
+
 	}
 
 	//Function to return an iterator at the beginning of the linked list
 	//Postcondition: Returns an iterator such that current is set to first
 	LinkedListIterator<Type> Begin()
 	{
-		
+		LinkedListIterator<Type>* temp;
+		temp->current = first;
+		return temp;
 	}
 
 	//Function to return an iterator at the end of the linked list
 	//Postcondition: Returns an iterator such that current is set to NULL
 	LinkedListIterator<Type> End()
 	{
-		
+		LinkedListIterator<Type>* temp;
+		temp->current = last;
+		return temp;
 	}
 
 	//Default constructor
@@ -140,28 +143,28 @@ public:
 	//Postcondition: first = null, last = null, count = 0
 	LinkedListType()
 	{
-		
+
 	}
 
 	//Copy constructor
-	LinkedListType(const LinkedListType<Type>&)
+	LinkedListType(const LinkedListType<Type> &rhs)
 	{
-		
+
 	}
 
 	//Destructor
 	//Deletes all the nodes from the list
-	//Postcondition: the list objects is destroyed
+	//Postcondition: the list objects are destroyed
 	~LinkedListType()
 	{
-		
+
 	}
 
 private:
 	//Function to make a copy of otherList
 	//Postcondition: a copy of otherList is created and assigned to this list
-	void CopyList(const LinkedListType<Type>&)
+	void CopyList(const LinkedListType<Type> &rhs)
 	{
-		
+
 	}
 };
